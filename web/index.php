515c29153b3b645d51792478bc04d0919c134dfc
charperashton <charperashton@salmon.com>
2017-05-31 13:40:32 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,29 +1,37 @@
 
 Back-end data for flight checks (small sample just for demo purposes of back-end integration)
 
-flight-number	depart	from	to	eta
-UA 234	13:40	New York	Chicago	15:30
-UA 340	14:00	New York	Seattle	19:05
-UA 342	15:20	Los Angeles	Boston	20:35
-UA 22	11:40	Orlando	San Fransisco	16:35
-UA 102	12:45	Houston	Tampa	18:20
 
 <TABLE>
    <TR>
-      <TD><p>flight-number</p></TD>
-      <TD>UA 234</TD>
-      <TD>UA 340</TD>
-      <TD>UA 342</TD>
-      <TD>UA 22</TD>
-      <TD>UA 102</TD>
-   </TR>
-   <TR>
-      <TD>depart</TD>
-      <TD>Data 4</TD>
-   </TR>
-   <TR>
+      <TD>flight-number</TD>
       <TD>from</TD>
-      <TD>Data 6</TD>
+      <TD>to</TD>
+   </TR>
+   <TR>
+      <TD>UA 234</TD>
+      <TD>New York</TD>
+      <TD>Chicago</TD>
+   </TR>
+   <TR>
+      <TD>UA 340</TD>
+      <TD>New York</TD>
+      <TD>Seattle</TD>
+   </TR>
+   <TR>
+      <TD>UA 342</TD>
+      <TD>Los Angeles</TD>
+      <TD>Boston</TD>
+   </TR>
+   <TR>
+      <TD>UA 22</TD>
+      <TD>Orlando</TD>
+      <TD>San Fransisco</TD>
+   </TR>
+   <TR>
+      <TD>UA 102</TD>
+      <TD>Houston</TD>
+      <TD>Tampa</TD>
    </TR>
 </TABLE>
 
